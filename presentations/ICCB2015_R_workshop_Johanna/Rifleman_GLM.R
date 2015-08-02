0d249b0f1af66c773653eb878475160250a03e53
Olivia Burge <olivia.burge@gmail.com>
2015-08-02 06:39:00 +0200

    post workshop updates

--- presentations/ICCB2015_R_workshop_Johanna/Rifleman_GLM.R
+++ presentations/ICCB2015_R_workshop_Johanna/Rifleman_GLM.R
@@ -3,20 +3,19 @@ library("coefplot")
 
 
 
-tomtit_data <- read.csv("D:/Bibliotheken/Documents/ICCB2015_R_workshop_Johanna/Tomtit_data.csv")
+tomtit_data <- read.csv("ICCB2015_R_workshop_Johanna/Tomtit_data.csv")
 
 tomtit_data
 # summary(Rif_data)
 
-tomtit.model <- lm(tomtit_abundance ~ habitat_type, tomtit_data)
+tomtit.model <- glm(tomtit_abundance ~ habitat_type, tomtit_data, family = "poisson")
 # tomtit.model2 <- lm (tomtit_abundance ~ habitat_type, family=poisson, tomtit_data)
 
 summary(tomtit.model)
 
 
-coefplot(tomtit.model)
+coefplot2(tomtit.model, intercept = TRUE)
 
-plot(fitted(tomtit.model),residuals(tomtit.model))
 
 hist(residuals(tomtit.model))
 
